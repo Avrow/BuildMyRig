8c8e4df73d793b3573f3659adb43ed31d0256b7d
Avrow <dinamitesavrow@gmail.com>
2026-05-05 01:12:07 +0600

    corsedit

--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "http://localhost:3001"],
+		origin: ["http://localhost:3000", "http://localhost:3001" ,"https://build-my-rig-two.vercel.app"],
 		credentials: true,
 	}),
 );
